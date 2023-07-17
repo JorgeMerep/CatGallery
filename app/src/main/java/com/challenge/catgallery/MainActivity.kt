e9f585f6f9fcd10c1576564b12da2b477e3a65ec
Jorge Merep <jorge221810@gmail.com>
2023-07-17 13:57:56 -0300

    Version Final

--- app/src/main/java/com/challenge/catgallery/MainActivity.kt
+++ app/src/main/java/com/challenge/catgallery/MainActivity.kt
@@ -20,6 +20,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         ApiClient.initialize(this)
 
         val breedEditText = findViewById<EditText>(R.id.breedEditText)
